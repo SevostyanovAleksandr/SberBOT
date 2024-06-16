84e0e719331800c57a2ac8e9ad7cf69128103400
SevostyanovAleksandr <74088145+SevostyanovAleksandr@users.noreply.github.com>
2024-06-16 21:42:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const bot = new TelegramBot(token, {
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Добрый день, Сбарахолка на связи! Для попадания в канал вы должны быть сотрудником СБЕРа и написать сюда ⚡️В ОДНОМ СООБЩЕНИИ!⚡️  ФИО и сберовскую почту . (ДЗО тоже подходит) 
-
-Если вдруг, бот говорит, что вы не то отправляете, подайте заново! Нужно всего одно сообщение с ФИО и почтой!');
+    bot.sendMessage(chatId, 'Добрый день, Сберахолка на связи! \nДля попадания в канал вы должны быть сотрудником СБЕРа и написать сюда \n⚡️В ОДНОМ СООБЩЕНИИ!⚡️  фамилии имени отчетсва и сберовскую почту . (ДЗО тоже подходит)  \nЕсли вдруг, бот говорит, что вы не то отправляете, подайте заново! Нужно всего одно сообщение с ФИО и почтой!');
 });
 
 bot.on('message', (msg) => {
@@ -20,12 +18,12 @@ bot.on('message', (msg) => {
 
     if (messageArray.length === 4) {
         const [firstName, lastName, otchestvo, email] = messageArray;
-        const message = `Данные приняты! ФИО: ${firstName} ${lastName} ${otchestvo} Почта: ${email}`;// Замените @username на фактическое имя пользователя
+        const message = `Данные приняты! \nФИО: ${firstName} ${lastName} ${otchestvo} \nПочта: ${email}`;// Замените @username на фактическое имя пользователя
         bot.sendMessage(chatId , message)
 
 
     } else {
-        bot.sendMessage(chatId, 'Неверный формат! Пожалуйста, введите данные в формате ФИО и внешней почты');
+        bot.sendMessage(chatId, 'Неверный формат! \nПожалуйста, введите данные в формате ФИО и внешней почты');
     }
 });
 
@@ -34,26 +32,3 @@ bot.onText(/\/help/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Доступные команды: /start, /help');
 });
-
-// const fetch = import('node-fetch');
-
-// const BOT_TOKEN = '7035543762:AAGR1qM7bt73_G4Pd4QZUF-lCGZUAB5xmXA';
-// const CHAT_ID = '@ENigmaA9'; // ID пользователя или группы
-
-// async function sendMessage(message) {
-//   const url = https://api.telegram.org/bot${BOT_TOKEN}/sendMessage;
-//   const params = new URLSearchParams({
-//     chat_id: CHAT_ID,
-//     text: message,
-//   });
-
-//   try {
-//     const response = await (await fetch).default(${url}?${params.toString()});
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.error('Error sending message:', error);
-//   }
-// }
-
-// sendMessage('Привет, это сообщение отправлено с помощью бота Telegram!');
