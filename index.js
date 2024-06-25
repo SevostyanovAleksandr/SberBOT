f98a16a2c1a8da2efe89a9ae09f39df7d9da39b8
Senior_Pomidor_Developer <sevoalex@mail.ru>
2024-06-25 17:39:25 +0300

    добавление функционала

--- index.js
+++ index.js
@@ -3,55 +3,105 @@ const token = '7035543762:AAGR1qM7bt73_G4Pd4QZUF-lCGZUAB5xmXA';
 const bot = new TelegramBot(token, {
     polling: true
 });
+const adminChatId = '@Sevotonya';
+const waitingForInput = new Map();
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привет! Пожалуйста, введите свое имя в формате: Имя и Фамилия');
+    bot.sendMessage(chatId, 'Добрый день, Сберахолка на связи! \nДля попадания в канал вы должны быть сотрудником СБЕРа и написать сюда \n⚡️В ОДНОМ СООБЩЕНИИ!⚡️ фамилии имени отчества и сберовскую почту. (ДЗО тоже подходит) \nЕсли вдруг, бот говорит, что вы не то отправляете, подайте заново! Нужно всего одно сообщение с ФИО и почтой!');
+    waitingForInput.set(chatId, true); // Устанавливаем флаг ожидания ввода
 });
 
+// Обработка команды /help
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    const helpMessage = 'Привет! Чтобы начать создание объявления - используй кнопку [Новое объявление] или команду /create. \nЯ предложу наполнить объявление текстом или фото. При этом новый текст будет заменять предыдущий, а не дополнять.Также у Телеграм есть ограничение на 10 фото в одном сообщении, поэтому не получится опубликовать объявление с большим количеством фото. \nУдалять фото нельзя, поэтому, если случайно добавлено не то фото, следует начать создание объявления заново. \nЧтобы опубликовать объявление - используй кнопку [Опубликовать] или команду /public. \nПеред публикацией я создам макет объявления и предложу подтвердить публикацию. Если все ок - жми [Да]. \nПомимо этого я пришлю сообщение об успешной публикации с ссылкой на объявление и с кнопкой его удаления. К сожалению, Телеграм не позволяет ботам удалять сообщения старше 48 часов.';
+    bot.sendMessage(chatId, helpMessage);
+});
+
+// Обработка команды /create
+bot.onText(/\/create/, (msg) => {
+    const chatId = msg.chat.id;
+    // Добавьте здесь логику для обработки команды /create
+    const options = {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                    text: 'Куплю/Продам',
+                    callback_data: 'data1'
+                }],
+                [{
+                    text: 'Сдам/Сниму',
+                    callback_data: 'data2'
+                }],
+                [{
+                    text: 'Посто вопросик',
+                    callback_data: 'data3'
+                }],
+                [{
+                    text: 'Отдам бесплатно',
+                    callback_data: 'data4'
+                }]
+            ]
+        }
+    };
+
+    bot.sendMessage(chatId, 'Выберите одну из опций:', options);
+    bot.on('callback_query', (callbackQuery) => {
+        const message = callbackQuery.message;
+
+        // Отправляем вопрос "Назови свою цену в рублях"
+        bot.sendMessage(message.chat.id, 'Назови свою цену в рублях:').then(() => {
+            // Ожидаем ответ от пользователя
+            bot.once('message', (msg) => {
+                const price = msg.text;
+                
+                bot.sendMessage(message.chat.id, 'Присылай описание или фотки. Не забудь выбрать теги ниже').then(() => {
+                    // Ожидаем ответ от пользователя
+                    bot.once('message', (msg) => {
+                        const description = msg.text;
+        
+                    });
+                });
+
+
+            });
+        });
+    });
+
+});
+// Обработка команды /public
+bot.onText(/\/public/, (msg) => {
+    const chatId = msg.chat.id;
+    // Добавьте здесь логику для обработки команды /public
+    bot.sendMessage(chatId, 'Команда /public была вызвана. Здесь будет ваша логика.');
+});
+
+// Обработка сообщений пользователей
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const messageText = msg.text;
-    const messageArray = messageText.split(' ');
-   // const user_id = '@ENigmaA9';
 
+    // Проверяем, ожидается ли ввод от пользователя и не является ли сообщение командой
+    if (waitingForInput.get(chatId) && !messageText.startsWith('/')) {
+        const messageArray = messageText.split(' ');
 
-    if (messageArray.length === 2) {
-        const [firstName, lastName] = messageArray;
-        const message = `Данные приняты! Имя: ${firstName} Фамилия: ${lastName}`;; // Замените @username на фактическое имя пользователя
-        bot.sendMessage(chatId , message)
+        if (messageArray.length >= 4) {
+            const firstName = messageArray[0];
+            const lastName = messageArray[1];
+            const otchestvo = messageArray[2];
+            const email = messageArray.slice(3).join(' '); // Объединяем остальные слова в email
+            const responseMessage = `Данные приняты, ожидайте проверки данных администратором! \nФИО: ${firstName} ${lastName} ${otchestvo} \nПочта: ${email}`;
+            bot.sendMessage(chatId, responseMessage);
+            waitingForInput.set(chatId, false); // Сбрасываем флаг ожидания ввода
 
-
-    } else {
-        bot.sendMessage(chatId, 'Неверный формат! Пожалуйста, введите данные в формате: Имя Фамилия');
+            // Отправляем данные администратору
+            const adminMessage = `Пользователь ${chatId} ввел следующие данные:\nФИО: ${firstName} ${lastName} ${otchestvo}\nПочта: ${email}`;
+            bot.sendMessage(adminChatId, adminMessage).catch(error => {
+                console.error(`Не удалось отправить сообщение администратору: ${error}`);
+            });
+        } else {
+            bot.sendMessage(chatId, 'Неверный формат! \nПожалуйста, введите данные в формате ФИО и внешней почты');
+        }
     }
 });
-
-
-bot.onText(/\/help/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Доступные команды: /start, /help');
-});
-
-// const fetch = import('node-fetch');
-
-// const BOT_TOKEN = '7035543762:AAGR1qM7bt73_G4Pd4QZUF-lCGZUAB5xmXA';
-// const CHAT_ID = '@ENigmaA9'; // ID пользователя или группы
-
-// async function sendMessage(message) {
-//   const url = https://api.telegram.org/bot${BOT_TOKEN}/sendMessage;
-//   const params = new URLSearchParams({
-//     chat_id: CHAT_ID,
-//     text: message,
-//   });
-
-//   try {
-//     const response = await (await fetch).default(${url}?${params.toString()});
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.error('Error sending message:', error);
-//   }
-// }
-
-// sendMessage('Привет, это сообщение отправлено с помощью бота Telegram!');
